7953e2ec03d5421f3dcf6a371fa378c51eac4b1e
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-07-20 01:22:39 +0900

    210719_콜라이더리지드적용_물뿌리개X

---
+++ Assets/Scripts/PourDetector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+            if(isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+
+    }
+
+    private void StartPour()
+    {
+        print("Fuck!!! start!!!!!!!!!!!!!!!!!");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("END");
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+}
