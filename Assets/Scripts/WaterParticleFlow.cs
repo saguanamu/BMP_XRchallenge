cd54d2d0c0616cba585b0094f1697c67f811ec57
byunyc0124 <baeyonyk124@gmail.com>
2021-07-26 17:45:12 +0900

    af

---
+++ Assets/Scripts/WaterParticleFlow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterParticleFlow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem ps;
+    //Rigidbody rg;
+
+    private bool isClick;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        //rg = GetComponent<Rigidbody>();
+        isClick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("´©¸§");
+            ps.Play(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("¼Õ¶À");
+            ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+}
