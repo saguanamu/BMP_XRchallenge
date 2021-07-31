6ddc119d9c41264e1fa9ac6423ecdedf6c93c61b
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-07-31 16:15:21 +0900

    rig set up

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,17 @@ public class PlayerMovement : MonoBehaviour
 
     Animator animator;
 
-    private InputDevice targetDevice;
+    //private InputDevice targetDevice;
+
+    public int speedForward = 12;
+    public int speedSide = 6;
+
+    private float dirX = 0;
+    private float dirZ = 0;
 
     void Start()
     {
+        /*
         List<InputDevice> devices = new List<InputDevice>();
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
@@ -31,15 +38,45 @@ public class PlayerMovement : MonoBehaviour
         {
             targetDevice = devices[0];
         }
+        */
+        characterBody = GetComponent<Transform>();
     }
 
     void Update()
     {
-       
+        MovePlayer();
     }
 
-    /*private void Move()
+
+
+    void MovePlayer()
     {
-       
-    }*/
+        dirX = 0;
+        dirZ = 0;
+        if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
+        {
+            Vector2 coord = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+            var absX = Mathf.Abs(coord.x);
+            var absY = Mathf.Abs(coord.y);
+
+            if(absX > absY)
+            {
+                if (coord.x > 0)
+                    dirX = +1;
+                else
+                    dirX = -1;
+            }
+            else
+            {
+                if (coord.y > 0)
+                    dirZ = +1;
+                else
+                    dirZ = -1;
+            }
+        }
+        Vector3 moveDir = new Vector3(dirX * speedSide, 0, dirZ * speedForward);
+        transform.Translate(moveDir * Time.smoothDeltaTime);
+
+    }
 }
