5cca72a6c508773cc8fbf127713fa77e5091aa6d
hyokyeoung <jis606124@gmail.com>
2021-08-04 18:35:22 +0900

    kimnongbu 쓰세여

---
+++ Assets/Shaders/Scripts/PourDetector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold; // T
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+            
+            if (isPouring)
+            {
+                print(CalculatePourAngle());
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+
+    }
+
+    private void StartPour()
+    {
+        print("start!!!!!!!!!!!!!!!!!");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("END");
+        currentStream = null;
+        currentStream.End();
+        
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+}
