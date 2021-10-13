4482f91e598846e25e692c7895e510326a4c0b80
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-10-14 08:00:48 +0900

    stage1 bgm + watering effect

---
+++ Assets/Script/SoundManger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManger : MonoBehaviour
+{
+    public AudioSource bgSound;
+    public static SoundManger instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound");
+        AudioSource audioSource = go.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+        Destroy(go, clip.length);
+    }
+    public void BgSoundPlay(AudioClip clip)
+    {
+        bgSound.clip = clip;
+        bgSound.loop = true;
+        bgSound.volume = 0.1f;
+        bgSound.Play();
+    }
+}
---
+++ Assets/Script/Stage1GrabObjectInteraction.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Stage1GrabObjectInteraction : MonoBehaviour
+{
+    //private XRGrabInteractable grab = null;
+    public XRController controller = null;
+    public AudioClip clip;
+
+    // water animation
+    private bool isWatered = false; // 물 뿌린 상태 초기값 false
+    [SerializeField] ParticleSystem ps;
+    public GameObject glow;
+
+    private void Update()
+    {
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+        {
+            Destroy(glow);
+            if (isWatered != primary)
+            {
+                isWatered = primary; // button on trigger
+                if (isWatered)
+                {
+                    ps.Play();
+                    SoundManger.instance.SFXPlay("liquid", clip);
+                }
+                else
+                {
+                    ps.Stop();
+                }
+            }
+        } 
+    }
+
+    
+}
