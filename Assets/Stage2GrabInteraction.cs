35da293deefda8851f0ec26cdcf59d436bd090f5
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-10-17 18:11:39 +0900

    s2 fin

--- Assets/Stage2GrabInteraction.cs
+++ Assets/Stage2GrabInteraction.cs
@@ -9,34 +9,67 @@ public class Stage2GrabInteraction : MonoBehaviour
     public XRController controller = null;
     public Animator animator = null;
     private bool isDowned = false; // 버튼 초기 상태
+    public GameObject glow;
 
     // curtain
-    private bool isClosed = false; // 왼쪽
-    public Animator curtain_animator = null;
+    public Transform other;
+    private bool Close = false; // 왼쪽
+    private bool Close2 = false; // 오른쪽
+    private Animator curtain_animator = null;
+    private Animator curtain_animator2 = null;
     
-
-
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        curtain_animator = GameObject.Find("curtain_1").GetComponent<Animator>();
+        curtain_animator2 = GameObject.Find("curtain_2").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
         {
-            if (isDowned != primary)
-            {
-                isDowned = primary; // button on trigger
-                if (isDowned)
-                {
-                    animator.SetBool("isDown", true);
+            if(other) {
+                float dist = Vector3.Distance(other.position, transform.position);
+                Debug.Log("Distance to other : " + dist);
+                if(dist <= 0.1f && CompareTag("L1")) {
+                    Destroy(glow);
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+                            animator.SetBool("isDown", true);
+                            curtain_animator.SetBool("Close", true);
+                        }
+                        else
+                        {
+                            animator.SetBool("isDown", false);
+                            curtain_animator.SetBool("Close", false);
+                        }
+                    }
                 }
-                else
+
+                if (dist <= 0.1f && CompareTag("L2"))
                 {
-                    animator.SetBool("isDown", false);
+                    Destroy(glow);
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+                            animator.SetBool("isDown", true);
+                            curtain_animator2.SetBool("Close2", true);
+                        }
+                        else
+                        {
+                            animator.SetBool("isDown", false);
+                            curtain_animator2.SetBool("Close2", false);
+                        }
+                    }
                 }
             }
         }
