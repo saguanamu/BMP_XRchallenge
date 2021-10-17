fbfd59cd08a805a134ea7eecca06212ad877496d
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-10-17 20:23:35 +0900

    stage6

--- Assets/Stage6GrabInteraction.cs
+++ Assets/Stage6GrabInteraction.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Stage6GrabInteraction : MonoBehaviour
 {
+    public XRController controller = null;
+    private bool isDowned = false; // 버튼 초기 상태
+    public GameObject glow;
+    public Transform other;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,89 @@ public class Stage6GrabInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (other)
+        {
+            float dist = Vector3.Distance(other.position, transform.position);
+            Debug.Log("Distance to other : " + dist);
+            if (dist <= 0.05f && CompareTag("Plant1"))
+            {
+                Destroy(glow);
+                if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+                {
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            if (dist <= 0.05f && CompareTag("Plant2"))
+            {
+                Destroy(glow);
+                if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+                {
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            if (dist <= 0.05f && CompareTag("Plant3"))
+            {
+                Destroy(glow);
+                if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+                {
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            if (dist <= 0.05f && CompareTag("Plant4"))
+            {
+                Destroy(glow);
+                if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+                {
+                    if (isDowned != primary)
+                    {
+                        isDowned = primary; // button on trigger
+                        if (isDowned)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
